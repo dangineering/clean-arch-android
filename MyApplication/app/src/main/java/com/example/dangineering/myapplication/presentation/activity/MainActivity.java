b0ec1120710de13baa96874016448be2b947f99b
daniel park <choijungah12@gmail.com>
2016-06-29 15:28:57 -0700

    init pckg

--- MyApplication/app/src/main/java/com/example/dangineering/myapplication/presentation/activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dangineering/myapplication/presentation/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.dangineering.myapplication;
+package com.example.dangineering.myapplication.presentation.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dangineering.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
